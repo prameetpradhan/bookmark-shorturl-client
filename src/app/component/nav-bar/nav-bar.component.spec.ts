e1ace78649b2b008e6decc5837e77cf31ca001de
Prameet <prameetpradhan@gmail.com>
2020-09-06 15:33:19 +0530

    test fix

--- src/app/component/nav-bar/nav-bar.component.spec.ts
+++ src/app/component/nav-bar/nav-bar.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavBarComponent } from './nav-bar.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
@@ -8,7 +11,17 @@ describe('NavBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavBarComponent ]
+      declarations: [ NavBarComponent ],
+      imports: [ TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient],
+        },
+        defaultLanguage: 'en'
+      }),
+      HttpClientModule
+     ]
     })
     .compileComponents();
   }));
@@ -23,3 +36,8 @@ describe('NavBarComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
