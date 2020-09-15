8efbf874e4bace80282884479ba7e967e8fac144
Prameet <prameetpradhan@gmail.com>
2020-09-15 12:13:10 +0530

    users url

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -30,7 +30,7 @@ export class AuthService {
   }
 
   isAuthorized(): boolean{
-    return this.jwtTokenService.isTokenExpired();
+    return !this.jwtTokenService.isTokenExpired();
   }
 
   logout(): void{
